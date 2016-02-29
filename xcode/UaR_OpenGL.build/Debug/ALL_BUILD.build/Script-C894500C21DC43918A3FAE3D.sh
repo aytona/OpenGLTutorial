0677c91b49e7d2bbcc5595b4b408ec23685d20a5
Christopher Aytona <christopher.aytona@gmail.com>
2016-02-29 10:45:17 -0500

    installed glew, and added project

---
+++ xcode/UaR_OpenGL.build/Debug/ALL_BUILD.build/Script-C894500C21DC43918A3FAE3D.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/Aytona/documents/GitHub/OpenGL_Tutorial/xcode -f /Users/Aytona/documents/GitHub/OpenGL_Tutorial/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
