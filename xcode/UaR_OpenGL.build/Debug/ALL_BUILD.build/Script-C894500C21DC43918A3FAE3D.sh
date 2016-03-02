5163b9a84b30af40967b7917bfb8e2416c963704
Christopher Aytona <christopher.aytona@gmail.com>
2016-03-01 22:46:04 -0500

    removed examples and remaking project

--- xcode/UaR_OpenGL.build/Debug/ALL_BUILD.build/Script-C894500C21DC43918A3FAE3D.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Aytona/documents/GitHub/OpenGL_Tutorial/xcode -f /Users/Aytona/documents/GitHub/OpenGL_Tutorial/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- xcode/UaR_OpenGL.build/Debug/Version.build/Objects-normal/x86_64/Version.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Aytona/Documents/GitHub/OpenGL_Tutorial/examples/Version.cpp \
-  /usr/local/include/GL/glew.h \
-  /Users/Aytona/documents/GitHub/OpenGL_Tutorial/glfw/include/GLFW/glfw3.h
