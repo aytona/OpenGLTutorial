305ade70d6273f3146b382017ddd61e14e6489a5
Christopher Aytona <christopher.aytona@gmail.com>
2016-03-04 23:50:55 -0500

    Final.cpp added

---
+++ examples/Final.cpp
@@ -0,0 +1,18 @@
+//
+//  Final.cpp
+//  UaR_OpenGL
+//
+//  Created by Christopher Aytona on 2016-03-04.
+//
+//
+
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include "GLFW/glfw3.h"
+
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
